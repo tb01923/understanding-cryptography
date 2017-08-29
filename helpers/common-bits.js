5d8903d51cd7f080d3a3328405490135babc0c07
todd <tbrown@frtservices.com>
2017-08-29 18:17:06 -0400

    hindley miln and unit tests on common-bits

--- helpers/common-bits.js
+++ helpers/common-bits.js
@@ -1,46 +1,67 @@
-const {reduce, pipe, curry} = require('./functional-bits')
+const {pipe, curry} = require('./functional-bits')
 
+// maxOr :: Number -> [Number] -> Number
 const maxOr = curry( (def, arr) =>
     (arr.length > 0) ?
         Math.max.apply(null, arr) :
         def
 )
 
+// appendToObj :: {...} -> String -> a -> {String: a, ...}
 const appendToObj = curry((obj, field, value) => {
     obj[field] = value
     return obj
 })
 
+// appendToArray :: [a] -> b -> [a,...b]
 const appendToArray = (arr, x) => {
     arr.push(x)
     return arr
 }
 
+// increment :: Number -> Number
 const increment = x => x +1
 
-const quotient = (dividend,divisor) => Math.floor(dividend/divisor)
-const floor_modulo = (dividend,divisor) => dividend - divisor * quotient(dividend,divisor)
+// quotient :: (Number, Number) -> Number
+const quotient = (dividend, divisor) => Math.floor(dividend / divisor)
 
+// floor_modulo :: (Number, Number) -> Number
+const floor_modulo = (dividend, divisor) => dividend - divisor * quotient(dividend, divisor)
 
+// values :: {k: v} -> [v]
 const values = (o) => Object.values(o)
 
+// values :: {k: v} -> [String]
 const keys = (o) => Object.keys(o)
 
+// join :: [] -> String
 const join  = arr => arr.join('')
 
+// join :: [] -> Number
 const length = arr => arr.length
 
+// numberOfKeys :: {} -> Number
 const numberOfKeys = pipe([
     keys,
     length
 ])
 
-const  gcd = function(a, b) {
-    if ( ! b) {
-        return a;
-    }
+// gcd :: (Number, Number) -> Number
+const  gcd = (a, b) =>
+    (!b) ?
+        a :
+        gcd(b, a % b);
 
-    return gcd(b, a % b);
-};
-
-module.exports = {maxOr, increment, appendToObj, appendToArray, floor_modulo, values, keys, join, length, numberOfKeys, gcd}
+module.exports = {
+    maxOr,
+    increment,
+    appendToObj,
+    appendToArray,
+    floor_modulo,
+    values,
+    keys,
+    join,
+    length,
+    numberOfKeys,
+    gcd
+}
---
+++ test/helpers/common-bits.test.js
@@ -0,0 +1,106 @@
+const mocha = require('mocha')
+    , chai = require('chai')
+    , should = chai.should() ;
+
+const { maxOr, increment, appendToObj, appendToArray, floor_modulo,
+    values, keys, join, length, numberOfKeys, gcd } = require('../../helpers/common-bits')
+
+describe('common-bits', () => {
+    describe('maxOr', () => {
+        it('maxOr should return the max of an array not default value', () => {
+            maxOr(-1, [10,22,10]).should.be.equal(22) ;
+        })
+        it('maxOr should return the default value when an empty array is specified', () => {
+            maxOr(-1, []).should.be.equal(-1) ;
+        })
+    })
+    describe('increment', () => {
+        it('increment should return NaN if it is not passed anything', () => {
+            increment().should.be.NaN ;
+        })
+        it('increment should increment an Number by adding 1', () => {
+            increment(2).should.be.equal(3) ;
+        })
+    })
+    describe('appendToObject', () => {
+        it('should append a value to an empty object', () => {
+            appendToObj({}, 'a', 1).should.have.a.property('a').equal(1);
+        })
+        it('should append a value to an existing object', () => {
+            appendToObj({'b': 3}, 'a', 1).should.have.a.property('a').equal(1);
+            appendToObj({'b': 3}, 'a', 1).should.have.a.property('b').equal(3);
+        })
+        it('should overwrite a value in an object that already has that property', () => {
+            appendToObj({'a': 3}, 'a', 1).should.have.a.property('a').equal(1);
+        })
+    })
+    describe('appendToArray', () => {
+        it('should append a value to an empty array', () => {
+            appendToArray([], 1).should.have.a.lengthOf(1).and.include(1);
+        })
+        it('should append a value to an non empty array', () => {
+            appendToArray([1], 2).should.have.a.lengthOf(2).and.include(1);
+            appendToArray([1], 2).should.have.a.lengthOf(2).and.include(2);
+        })
+    })
+    describe('floor_modulo', () => {
+        it('should work on positive numbers', () => {
+            floor_modulo(10,3).should.equal(1);
+        })
+        it('should work on negative numbers', () => {
+            floor_modulo(-10,3).should.not.equal(1);
+            floor_modulo(-10,3).should.equal(2);
+        })
+    })
+    describe('values', () => {
+        it('should return an empty array on an empty pojso', () => {
+            values({}).should.be.an('array').with.a.lengthOf(0);
+        })
+        it('should return all the property values iof an pojso', () => {
+            values({'a': 1, 'b': 2}).should.be.an('array').with.a.lengthOf(2);
+            values({'a': 1, 'b': 2}).should.include(1) ;
+            values({'a': 1, 'b': 2}).should.include(2);
+        })
+    })
+    describe('keys', () => {
+        it('should return an empty array on an empty pojso', () => {
+            keys({}).should.be.an('array').with.a.lengthOf(0);
+        })
+        it('should return all the property values iof an pojso', () => {
+            keys({'a': 1, 'b': 2}).should.be.an('array').with.a.lengthOf(2);
+            keys({'a': 1, 'b': 2}).should.include('a') ;
+            keys({'a': 1, 'b': 2}).should.include('b');
+        })
+    })
+    describe('join', () => {
+        it('should return an empty string when passed an empty array', () => {
+            join([]).should.equal('')
+        })
+        it('should return a string with all values concatenated', () => {
+            join([1,2,3]).should.equal('123')
+        })
+    })
+    describe('length', () => {
+        it('should return 0 when passed an empty array', () => {
+            length([]).should.equal(0)
+        })
+        it('should return with the length of the array', () => {
+            length([1,2,3]).should.equal(3)
+        })
+    })
+    describe('numberOfKeys', () => {
+        it('should count the number of properties in a pojso', () => {
+            numberOfKeys({}).should.equal(0)
+            numberOfKeys({'a': 1}).should.equal(1)
+            numberOfKeys({'a': 1, 'b': 1}).should.equal(2)
+        })
+    })
+    describe('gcd', () => {
+        it('should return 1 if there are no common divisors', () => {
+            gcd(13,17).should.equal(1)
+        })
+        it('should return a gcd if passed even numbers', () => {
+            gcd(20,10).should.equal(10)
+        })
+    })
+})
